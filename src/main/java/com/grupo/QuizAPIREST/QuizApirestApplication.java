58a4730e5b0d48bdf03af874f8568faf99c14ef0
Juan <molano.juan.2629@eam.edu.co>
2025-03-25 07:30:40 -0500

    JUAN MOLANO INITIAL COMMIT

---
+++ src/main/java/com/grupo/QuizAPIREST/QuizApirestApplication.java
@@ -0,0 +1,13 @@
+package com.grupo.QuizAPIREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo/QuizAPIREST/QuizApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo.QuizAPIREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
