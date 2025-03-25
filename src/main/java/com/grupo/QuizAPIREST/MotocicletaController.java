aa59a303cae5da641a817b2db6265859bfc71f7f
Juan <molano.juan.2629@eam.edu.co>
2025-03-25 08:25:05 -0500

    JUAN MOLANO AGREGADA funcionalidad para actualizar una motocicleta.

--- src/main/java/com/grupo/QuizAPIREST/MotocicletaController.java
+++ src/main/java/com/grupo/QuizAPIREST/MotocicletaController.java
@@ -9,7 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,5 +35,18 @@ public class MotocicletaController {
     public ResponseEntity<List<Motocicleta>> getAllMotocicletas() {
         List<Motocicleta> motocicletas = service.getAllMotocicletas();
         return new ResponseEntity<>(motocicletas, HttpStatus.OK);
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<Motocicleta> actualizarMotocicleta(@PathVariable String id,
+            @RequestBody Motocicleta motocicleta) {
+        Motocicleta mExistente = service.findById(id);
+        if (mExistente != null) {
+            motocicleta.setId(id);
+            Motocicleta mActualizada = service.update(motocicleta);
+            return new ResponseEntity<>(mActualizada, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/grupo/QuizAPIREST/MotocicletaRepository.java
+++ src/main/java/com/grupo/QuizAPIREST/MotocicletaRepository.java
@@ -26,4 +26,22 @@ public class MotocicletaRepository {
         motocicletas.add(m);
         return m;
     }
+    
+    public Motocicleta findById(String id) {
+        for (Motocicleta m : motocicletas) {
+            if (m.getId().equals(id)) {
+                return m;
+            }
+        }
+        return null;
+    }
+    
+    public Motocicleta update(Motocicleta motocicleta) {
+        for (Motocicleta m : motocicletas) {
+            if (m.getId().equals(motocicleta.getId())) {
+                m = motocicleta;
+            }
+        }
+        return null;
+    }
 }
